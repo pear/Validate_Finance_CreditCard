c436e070605380651157aaded250bab3fe1de8b7
Tomas V.V.Cox <cox@php.net>
2002-05-29 09:33:57 +0000

    Added number() tests
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@83984 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/number.php
@@ -0,0 +1,37 @@
+<?php
+require_once '../Validate.php';
+
+function test($res, $expected) {
+    static $no = 1;
+    if ($res !== $expected) {
+        echo "test $no failed\n";
+    }
+    $no++;
+}
+// 1
+test(Validate::number(8), true);
+// 2
+test(Validate::number('8'), true);
+// 3
+test(Validate::number('-8'), true);
+// 4
+test(Validate::number(-8), true);
+// 5
+test(Validate::number('-8,', ','), false);
+// 6
+test(Validate::number('-8.0', ','), false);
+// 7
+test(Validate::number('-8,0', ',', 2), true);
+// 8
+test(Validate::number(8.0004, '.', 3), false);
+// 9
+test(Validate::number(8.0004, '.', 4), true);
+// 10
+test(Validate::number('-8', null, null, 1, 9), false);
+// 11
+test(Validate::number('-8', null, null, -8, -7), true);
+// 12
+test(Validate::number('-8.02', '.', null, -8, -7), false);
+// 13
+test(Validate::number('-8.02', '.', null, -9, -7), true);
+?>
