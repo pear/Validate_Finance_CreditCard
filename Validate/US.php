3b2b14d5ee6c15047d3c57040f77a27e4dd4b48c
Pierre-Alain Joye <pajoye@php.net>
2003-02-16 17:25:09 +0000

    Add multiple support to ssn
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@116643 c90b9560-bf6c-de11-be94-00142212c4b1

--- Validate/US.php
+++ Validate/US.php
@@ -35,6 +35,9 @@ class Validate_US
      */
     function ssn($ssn, $high_groups = null)
     {
+		if(is_array($ssn)){
+			extract($ssn);
+		}
         // remove any dashes, spaces, returns, tabs or slashes
         $ssn = str_replace(array('-','/',' ',"\t","\n"), '', $ssn);
 
@@ -65,6 +68,9 @@ class Validate_US
     */
     function ssnGroupRange($groupNumber)
     {
+		if(is_array($groupNumber)){
+			extract($groupNumber);
+		}
         if ($groupNumber < 10) {
             // is the number odd?
             if ($groupNumber % 2) {
@@ -94,6 +100,9 @@ class Validate_US
      */
     function ssnCheck($area, $group, $serial, &$high_groups)
     {
+		if(is_array($area)){
+			extract($area);
+		}
         // perform trivial checks
         // no field should contain all zeros
         if (!($area && $group && $serial)) {
