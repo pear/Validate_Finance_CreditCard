a61dfbe13361874b1e97eb14b3af03b159a3c14e
Pierre-Alain Joye <pajoye@php.net>
2003-02-16 17:30:25 +0000

    initial commit, postalcode check
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@116646 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Validate/UK.php
@@ -0,0 +1,36 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2003 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.02 of the PHP license,      |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://www.php.net/license/2_02.txt.                                 |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Authors: Pierre-Alain Joye <paj@pearfr.org>                          |
+// |                                                                      |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+//
+// Specific validation methods for data used in UK
+//
+
+require_once 'Validate.php';
+
+class Validate_UK
+{
+    function valid_postcode_uk($code){
+        if(is_array($code)){
+            extract($code);
+        }
+        return (ereg ('^[A-Z]{1, 2}[0-9]{1, 2}[A-Z]{0, 1} [0-9][A-Z]{2}$', $str));
+    }
+}
+?>
