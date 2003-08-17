7fe536a64eeb2940c9cf1e27b55e9f15638cce33
Stefan Neufeind <neufeind@php.net>
2003-08-17 19:21:49 +0000

    Initial support for FINANCE; needed for 'multiple()' to validate an IBAN
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@138137 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Validate/FINANCE.php
@@ -0,0 +1,46 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | PHP version 4.0                                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2003 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.0 of the PHP license,       |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://www.php.net/license/2_02.txt.                                 |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Author: Stefan Neufeind <pear.neufeind@speedpartner.de>              |
+// +----------------------------------------------------------------------+
+
+require_once('Validate/Finance/IBAN.php');
+
+/**
+* Financial functions for validation and calculation
+*
+* @author      Stefan Neufeind <neufeind@speedpartner.de>
+* @version     0.1
+* @since       PHP 4.1.0
+*/
+class Validate_FINANCE {
+
+    /**
+     * Performs validation an IBAN (international bankaccoun number)
+     *
+     * @param     string      $iban              IBAN to be validated
+     * @access    public
+     * @since     0.1
+     * @return    boolean   true if IBAN is okay
+     */
+    function iban($iban='')
+    {
+        return Validate_Finance_IBAN::validate($iban);
+    } // end func iban
+
+    // }}}
+
+} // end class Validate_FINANCE
+?>
