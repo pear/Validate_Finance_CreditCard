dc3c304cefc424e76b704d36e77d8a3dc6c7d8d2
Dave Mertens <zyprexia@php.net>
2003-10-06 21:26:25 +0000

    Added some Dutch validation checks - postcode     support several formats such as '1234 AB' and '1234AB' where lowercase letters are allowed (1234ab) - phonenumber     with type checking:     - normal (vaste KPN net) (mobilenumbers not allowed)     - mobile (only mobile numbers are allowed)     - any (both normal and mobile numbers)
        Internatinal number format is also supported (0031, +31).
        Check is based on last 9 digits (so anything before is ignored)
    - Social security check (SoFi nummer)
    
    At some later point also bankaccount number check (both bank as Postbank)
    
    
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@141943 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Validate/NL.php
@@ -0,0 +1,95 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | PHP Version 4                                                        |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 1997-2003 The PHP Group                                |
+// +----------------------------------------------------------------------+
+// | This source file is subject to version 2.02 of the PHP license,      |
+// | that is bundled with this package in the file LICENSE, and is        |
+// | available at through the world-wide-web at                           |
+// | http://www.php.net/license/2_02.txt.                                 |
+// | If you did not receive a copy of the PHP license and are unable to   |
+// | obtain it through the world-wide-web, please send a note to          |
+// | license@php.net so we can mail you a copy immediately.               |
+// +----------------------------------------------------------------------+
+// | Authors: Dave Mertens <zyprexia@php.net>                             |
+// +----------------------------------------------------------------------+
+//
+// $Id$
+//
+// Specific validation methods for data used in NL
+//
+
+require_once('Validate.php');
+
+define( "PHONENUMBER_TYPE_ANY",     0);     //Any dutch phonenumber
+define( "PHONENUMBER_TYPE_NORMAL",  1);     //only normal phonenumber (mobile numers are not allowed)
+define( "PHONENUMBER_TYPE_MOBILE",  2);     //only mobile numbers are allowed
+
+class Validate_NL
+{
+    /**
+     * Validate a NL postcode
+     *
+     * @param   string  $postcode       NL postcode to validate
+     * @return  bool    true if postcode is ok, false otherwise
+     */
+    function postcode($postcode)
+    {
+        return (ereg('^[0-9]{4}\ {0,1}[A-Za-z]{2}$', $postcode)); // '1234 AB', '1234AB', '1234 ab'
+    }
+    
+    
+    
+    /** 
+     * Validate a phonenumber
+     *
+     * @param   string  $number         Dutch phonenumber (can be in international format (eg +31 or 0031)
+     * @param   int     $type           Type of phonenumber to check
+     * @return  bool    true if (phone) number is correct
+     */
+    function phonenumber($number, $type = PHONENUMBER_TYPE_ANY)
+    {
+        $result = false;
+        
+        //we need at least 9 digits
+        if (ereg("^[+0-9]{9,}$", $number)) {
+            $number = substr($number, strlen($number)-9);
+            
+            //we only use the last 9 digits (so no troubles with international numbers)
+            if (strlen($number) >= 9)) {
+                switch ($type) 
+                {
+                    case PHONENUMBER_TYPE_ANY:
+                        $result = true;     //we have a 9 digit numeric number.
+                        break;
+                    case PHONENUMBER_TYPE_NORMAL:
+                        if ((int)$number[0] != 6)
+                            $result = true;     //normal phonenumbers don't begin with 6 (00316, +316 and 06 are reserved for mobile numbers)
+                        break;
+                    case PHONENUMBER_TYPE_MOBILE:
+                        if ((int)$number[0] == 6)
+                            $result = true;     //mobilenumbers start with a 6
+                        break;
+                 }
+            }
+        }
+
+        return $result;
+    }
+    
+    
+    /**
+     * Social Security Number check (very simple, just a 9 digit number..)
+     * In Dutch SoFi (Sociaal Fiscaal) nummer
+     *
+     * @param   string  $number     Dutch social security number
+     * @return  bool    true if SSN number is correct
+     */
+    function SSN($number)
+    {
+        return (ereg("^[0-9]{9}$", $number));   
+    }
+}
+?>
