0c1d05be9e3ed469fbb886c69be34f9a3b9e4e0d
Michael Wallner <mike@php.net>
2003-12-10 16:22:56 +0000

    stripped some whitespace
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@146130 c90b9560-bf6c-de11-be94-00142212c4b1

--- Validate/AT.php
+++ Validate/AT.php
@@ -47,7 +47,7 @@ class Validate_AT
     function postcode($zip)
     {
         static $postcodes;
-        
+
         if (!isset($postcodes)) {
             $file = '@DATADIR@/Validate/AT_postcodes.txt';
             $postcodes = array_map('trim', file($file));
@@ -77,27 +77,27 @@ class Validate_AT
         if (!$matched) {
             return false;
         }
-        
+
         list(, $num, $chk, $d, $m, $y) = $matches;
 
         if (!Validate::date("$d-$m-$y", array('format' => '%d-%m-%y'))) {
             return false;
         }
-        
+
         $str = (string) $num . $chk . $d . $m . $y;
         $len = strlen($str);
         $fkt = '3790584216';
         $sum = 0;
-        
+
         for ($i = 0; $i < $len; $i++) {
             $sum += $str{$i} * $fkt{$i};
         }
-        
+
         $sum = $sum % 11;
         if ($sum == 10) {
             $sum = 0;
         }
-        
+
         return ($sum == $chk);
     }
 }
