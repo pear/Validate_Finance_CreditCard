06b8733e6f7ea1e29bd5260e31b618f2f736e92c
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2004-12-17 05:57:03 +0000

    Request #2194 Improved CreditCard validation with CreditCard Type check, patch by Philippe
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@174944 c90b9560-bf6c-de11-be94-00142212c4b1

--- Validate.php
+++ Validate.php
@@ -3,7 +3,7 @@
 // +----------------------------------------------------------------------+
 // | PHP Version 4                                                        |
 // +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2003 The PHP Group                                |
+// | Copyright (c) 1997-2004 The PHP Group                                |
 // +----------------------------------------------------------------------+
 // | This source file is subject to version 3.0 of the PHP license,       |
 // | that is bundled with this package in the file LICENSE, and is        |
@@ -185,34 +185,119 @@ class Validate
      * This function checks given number according Luhn check
      * algorithm. It is published on several places, also here:
      *
-     *      http://www.webopedia.com/TERM/L/Luhn_formula.html
-     *      http://www.merriampark.com/anatomycc.htm
-     *      http://hysteria.sk/prielom/prielom-12.html#3 (Slovak language)
-     *      http://www.speech.cs.cmu.edu/~sburke/pub/luhn_lib.html (Perl lib)
+     * @link http://www.webopedia.com/TERM/L/Luhn_formula.html
+     * @link http://www.merriampark.com/anatomycc.htm
+     * @link http://hysteria.sk/prielom/prielom-12.html#3 (Slovak language)
+     * @link http://www.speech.cs.cmu.edu/~sburke/pub/luhn_lib.html (Perl lib)
      *
-     * @param  string  $number number (only numeric chars will be considered)
-     * @return bool    true if number is valid, otherwise false
+     * @param  string  $number to check
+     * @return bool    TRUE if number is valid, FALSE otherwise
+     * @access public
      * @author Ondrej Jombik <nepto@pobox.sk>
      */
-    function creditCard($creditCard)
+    function Luhn($number)
     {
-        $creditCard = preg_replace('/[^0-9]/','',$creditCard);
-        if (empty($creditCard) || ($len_number = strlen($creditCard)) <= 0) {
-            return false;
-        }
+        $len_number = strlen($number);
         $sum = 0;
         for ($k = $len_number % 2; $k < $len_number; $k += 2) {
-            if ((intval($creditCard{$k}) * 2) > 9) {
-                $sum += (intval($creditCard{$k}) * 2) - 9;
+            if ((intval($number{$k}) * 2) > 9) {
+                $sum += (intval($number{$k}) * 2) - 9;
             } else {
-                $sum += intval($creditCard{$k}) * 2;
+                $sum += intval($number{$k}) * 2;
             }
         }
         for ($k = ($len_number % 2) ^ 1; $k < $len_number; $k += 2) {
-            $sum += intval($creditCard{$k});
+            $sum += intval($number{$k});
         }
-        return $sum % 10 ? false : true;
+        return ($sum % 10) ? false : true;
     }
+
+
+    /**
+     * Validate a number according to Luhn check algorithm
+     *
+     * If a type is passed, the number will be checked against it.
+     *
+     * @param  string  $creditCard number (only numeric chars will be considered)
+     * @param  string  $cardType card type ('visa', 'mastercard'...).
+     *                 Case-insensitive
+     * @return bool    TRUE if number is valid, FALSE otherwise
+     * @access public
+     * @see luhn()
+     * @author Ondrej Jombik <nepto@pobox.sk>
+     * @author Philippe Jausions <Philippe.Jausions@11abacus.com>
+     */
+    function creditCard($creditCard, $cardType = null)
+    {
+        $creditCard = preg_replace('/[^0-9]/', '', $creditCard);
+        if (empty($creditCard) || ($len_number = strlen($creditCard)) <= 0) {
+            return false;
+        }
+
+        // Only apply the Luhn algorithm for cards other than enRoute
+        // So check if we have a enRoute card now
+        if (strlen($creditCard) != 15
+            || (substr($creditCard, 0, 4) != '2014'
+                && substr($creditCard, 0, 4) != '2149')) {
+
+            if (!Validate::Luhn($creditCard)) {
+                return false;
+            }
+        }
+
+        if (!is_null($cardType)) {
+            return Validate::creditCardType($creditCard, $cardType);
+        }
+
+        return true;
+    }
+
+
+    /**
+     * Validates the credit card number against a type
+     *
+     * @param string $creditCard credit card number to check
+     * @param string $cardType Case-insensitive type (no spaces)
+     * @return boolean TRUE is type matches, FALSE otherwise
+     * @access public
+     * @author Philippe Jausions <Philippe.Jausions@11abacus.com>
+     * @link http://www.beachnet.com/~hstiles/cardtype.html
+     */
+    function creditCardType($creditCard, $cardType) {
+
+        switch (strtoupper($cardType)) {
+            case 'MASTERCARD':
+                $regex = '/^5[1-5]\d{14}$/';
+                break;
+            case 'VISA':
+                $regex = '/^4(\d{12}|\d{15})$/';
+                break;
+            case 'AMEX':
+            case 'AMERICANEXPRESS':
+                $regex = '/^3[47]\d{13}$/';
+                break;
+            case 'DINERS':
+            case 'DINERSCLUB':
+            case 'CARTEBLANCHE':
+                $regex = '/^3(0[0-5]\d{11}|[68]\d{12})$/';
+                break;
+            case 'DISCOVER':
+                $regex = '/^6011\d{12}$/';
+                break;
+            case 'JCB':
+                $regex = '/^(3\d{15}|(2131|1800)\d{11})$/';
+                break;
+            case 'ENROUTE':
+                $regex = '/^2(014|149)\d{11}$/';
+                break;
+            default:
+                return false;
+        }
+
+        $creditCard = preg_replace('/[^0-9]/', '', $creditCard);
+        return (bool) preg_match($regex, $creditCard);
+    }
+
 
     /**
      * Validate date and times. Note that this method need the Date_Calc class
@@ -742,4 +827,5 @@ class Validate
         return $valid;
     }
 }
+
 ?>
--- tests/credit_card.php
+++ tests/credit_card.php
@@ -7,11 +7,35 @@ class Validate_CreditCard extends PHPUnit_TestCase
     var $cards = array(
                     0   => false,
                     -1  => false,
-                    '6762195515061813'  => true,
-                    '6762195515061814'  => false,
-                    '4405771709596026'  => true,
+                    '6762195515061813'    => true,
+                    '6762 1955 1506 1813' => true,
+                    '6762195515061814'    => false,
+                    '4405771709596026'    => true,
                     'Big brother is watching you' => false
                 );
+
+    // Test cards
+    var $cardTypes = array(
+        'VISA'       => '4111 1111 1111 1111',
+        'VISA'       => '4111-1111-1111-1111',
+        'VISA'       => '4111111111111111',
+        'MasterCard' => '5500 0000 0000 0004',
+        'AMEX'       => '3400 0000 0000 009',
+        'JCB'        => '3088 0000 0000 0009',
+        'ENROUTE'    => '2014 0000 0000 009',
+        'DISCOVER'   => '6011 0000 0000 0004',
+        'DINERSCLUB' => '3000 0000 0000 04');
+
+    // Test valid LUHN, but invalid cards
+    var $shortCards = array(
+        'VISA'       => '41111',
+        'MasterCard' => '5413',
+        'AMEX'       => '3400009',
+        'JCB'        => '21311',
+        'ENROUTE'    => '2014009',
+        'DISCOVER'   => '60110004',
+        'DINERSCLUB' => '300004');
+
     function Validate_date( $name )
     {
         $this->PHPUnit_TestCase($name);
@@ -28,15 +52,34 @@ class Validate_CreditCard extends PHPUnit_TestCase
 
     function testCreditCard()
     {
-        foreach ($this->cards as $card=>$expected_result){
+        foreach ($this->cards as $card => $expected_result) {
             $r = Validate::creditCard($card);
             $this->assertEquals($r, $expected_result);
         }
     }
+
+    function testDetectCreditCardType()
+    {
+        foreach (array_keys($this->cardTypes) as $comp_type) {
+            foreach ($this->cardTypes as $type => $number) {
+                $r = Validate::creditCardType($number, $comp_type);
+                $this->assertEquals($r, (bool) ($type == $comp_type));
+            }
+        }
+    }
+
+    function testValidLuhnCheckBadNumberCreditCardType()
+    {
+        foreach ($this->shortCards as $type => $number) {
+            $r = Validate::creditCard($number, $type);
+            $this->assertEquals($r, false);
+        }
+    }
+
 }
 
 // runs the tests
-$suite = new PHPUnit_TestSuite("Validate_CreditCard");
+$suite = new PHPUnit_TestSuite('Validate_CreditCard');
 $result = PHPUnit::run($suite);
 // prints the tests
 echo $result->toString();
