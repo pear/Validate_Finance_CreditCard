83f58da62d0cbc359066e8e2798220f382507529
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2005-01-11 20:37:52 +0000

    add test for the email functions, please if anyone notices that any of the emails are wrongly validated then let me know! (as in if I told the test that it should be valid but it really isn't)
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@177001 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/email.php
@@ -0,0 +1,78 @@
+<?php
+require_once 'PHPUnit.php';
+require 'Validate.php';
+
+class Validate_Email_Test extends PHPUnit_TestCase
+{
+    var $email = array(
+        // Try dns lookup
+        array('example@example.org', true) => true,
+        array('example@fluffffffrefrffrfrfrfrfrfr.is', false) => false,
+        // with out the dns lookup
+        'example@fluffffffrefrffrfrfrfrfrfr.is' => true,
+        
+        // Some none english chars
+        'hæjjæ@homms.com' => true,
+
+        // Test for various ways with _
+        'mark_@example.com' => true,
+        '_mark@example.com' => true,
+        'mark_foo@example.com' => true,
+
+        // Test for various ways with -
+        'mark-@example.com' => true,
+        '-mark@example.com' => true,
+        'mark-foo@example.com' => true,
+
+        // Test for various ways with .
+        'mark.@example.com' => false,
+        '.mark@example.com' => false,
+        'mark.foo@example.com' => true,
+
+        // Test for various ways with ,
+        'mark,@example.com' => false,
+        ',mark@example.com' => false,
+        'mark,foo@example.com' => false,
+
+        // Test for various ways with :
+        'mark:@example.com' => false,
+        ':mark@example.com' => false,
+        'mark:foo@example.com' => false,
+
+        // Test for various ways with ;
+        'mark;@example.com' => false,
+        ';mark@example.com' => false,
+        'mark;foo@example.com' => false,
+
+        // Test for various ways with |
+        'mark|@example.com' => true,
+        '|mark@example.com' => true,
+        'mark|foo@example.com' => true,
+    );
+
+    function Validate_Email_Test($name)
+    {
+        $this->PHPUnit_TestCase($name);
+    }
+
+    function testEmail()
+    {
+        foreach ($this->email as $email => $expected_result) {
+            if (is_array($email)) {
+                $email = $email[0];
+                $bool = $email[1];
+            } else {
+                $bool = false;
+            }
+            $r = Validate::email($email, $bool);
+            $this->assertEquals($r, $expected_result);
+        }
+    }
+}
+
+// runs the tests
+$suite = new PHPUnit_TestSuite('Validate_Email_Test');
+$result = PHPUnit::run($suite);
+// prints the tests
+echo $result->toString();
+?>
