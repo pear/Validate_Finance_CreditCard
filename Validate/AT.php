d39dbfcd500b4d98efe92ad645cf71e9653a3d2b
Helgi Þormar Þorbjörnsson <dufuz@php.net>
2005-01-11 13:04:09 +0000

    space fixes
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@176957 c90b9560-bf6c-de11-be94-00142212c4b1

--- Validate/AT.php
+++ Validate/AT.php
@@ -54,7 +54,7 @@ class Validate_AT
                 $postcodes = array_map('trim', file($file));
             }
     
-            return in_array((int) $postcode, $postcodes);
+            return in_array((int)$postcode, $postcodes);
         }
         return (bool)ereg('^[0-9]{4}$', $postcode);
     }
