468b067314a132effd03b82a94c89157d3457738
Philippe Jausions <jausions@php.net>
2005-04-20 17:14:15 +0000

    Updated header comment blocks
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@184772 c90b9560-bf6c-de11-be94-00142212c4b1

--- Validate/Finance/CreditCard.php
+++ Validate/Finance/CreditCard.php
@@ -1,24 +1,50 @@
 <?php
-/* vim: set expandtab tabstop=4 shiftwidth=4: */
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2005 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 3.0 of the PHP license,       |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available through the world-wide-web at the following url:           |
-// | http://www.php.net/license/3_0.txt.                                  |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Ondrej Jombik <nepto@pobox.sk>                              |
-// |         Philippe Jausions <Philippe.Jausions@11abacus.com>           |
-// +----------------------------------------------------------------------+
-//
-// $Id$
 
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * Validation methods for credit card related data
+ *
+ * PHP versions 4 and 5
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Validate
+ * @package    Validate
+ * @subpackage Validate_Finance_CreditCard
+ * @author     Philippe Jausions <Philippe.Jausions@11abacus.com>
+ * @copyright  2005 The PHP Group
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    CVS: $Id$
+ * @link       http://pear.php.net/package/Validate
+ */
+
+/**
+ * Credit card related information validation class
+ *
+ * This class provides methods to validate:
+ *  - Credit card number
+ *  - Card security code
+ *  - Card type (i.e. Visa, Mastercard...)
+ *
+ * The methods only check the format of the data. For instance
+ * the package does NOT check if a card is a legitimate card registered
+ * with a card issuer, or if the card is reported stolen, etc...
+ *
+ * @category   Validate
+ * @package    Validate
+ * @subpackage Validate_Finance_CreditCard
+ * @author     Philippe Jausions <Philippe.Jausions@11abacus.com>
+ * @author     Ondrej Jombik <nepto@pobox.sk>
+ * @copyright  2005 The PHP Group
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    Release: @package_version@
+ * @link       http://pear.php.net/package/Validate
+ */
 class Validate_Finance_CreditCard
 {
     /**
