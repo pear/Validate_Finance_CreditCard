af1650eaceab33c45947a020c22cf20bb1045837
Pierre-Alain Joye <pajoye@php.net>
2005-10-05 05:20:18 +0000

    - Initial AU import
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@197603 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ Validate/AU.php
@@ -0,0 +1,296 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+
+/**
+ * Specific validation methods for data used in Australia
+ *
+ * Contains code from Validate_AT and Validate_UK
+ *
+ * PHP versions 4
+ *
+ * LICENSE: This source file is subject to version 3.0 of the PHP license
+ * that is available through the world-wide-web at the following URI:
+ * http://www.php.net/license/3_0.txt.  If you did not receive a copy of
+ * the PHP License and are unable to obtain it through the web, please
+ * send a note to license@php.net so we can mail you a copy immediately.
+ *
+ * @category   Validate
+ * @package    Validate_AU
+ * @author     Daniel O'Connor <daniel.oconnor@gmail.com>
+ * @author     Tho Nguyen <tho.nguyen@itexperts.com.au>
+ * @copyright  2005 The PHP Group
+ * @date       $Date$
+ * @version    $Id$
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @link       http://www.getfridged.com/validate/
+ */
+
+/**
+ * Requires Validate
+ */
+require_once 'Validate.php';
+
+/**
+ * Data validation class for Australia
+ *
+ * Contains code from Validate_AT and Validate_UK
+ *
+ * This class provides methods to validate:
+ *  - Postal code
+ *  - Phone number
+ *  - Australian Business Number
+ *  - Australian Company Number
+ *  - Tax File Number
+ *
+ * @category   Validate
+ * @package    Validate_AU
+ * @author     Daniel O'Connor <daniel.oconnor@gmail.com>
+ * @author     Tho Nguyen <tho.nguyen@itexperts.com.au>
+ * @copyright  2005 The PHP Group
+ * @license    http://www.php.net/license/3_0.txt  PHP License 3.0
+ * @version    Release: @package_version@
+ * @link       http://www.getfridged.com/validate/
+ */
+class Validate_AU
+{ 
+
+    /**
+     * Validate postcode
+     *
+     * @static
+     * @access   public
+     * @param    string  $postcode  postcode to validate
+     * @param    bool    $strong    optional; strong checks (e.g. against a list of postcodes)
+     * @return   bool    true if postcode is ok, false otherwise
+     */
+    function postalCode($postcode, $strong = false)
+    {
+        if ($strong) {
+            static $postcodes;
+    
+            if (!isset($postcodes)) {
+                $file = $_ENV['PHP_PEAR_DATA_DIR'].'/Validate_AU/AU_postcodes.txt';
+                $postcodes = array_map('trim', file($file));
+            }
+    
+            return in_array((int)$postcode, $postcodes);
+        }
+        return (bool)ereg('^[0-9]{4}$', $postcode);
+    }
+
+
+    /**
+     * Validate a telephone number.
+     *
+     * Be aware, 1800 XXX XXX, 190X XXX XXX, 13XXXX numbers
+     * are not catered for at this time.
+     *
+     * @static
+     * @access    public
+     * @param     string    $tel the tel number
+     * @return    bool
+     */
+    function tel($tel)
+    {
+        $tel = str_replace(array('(', ')', '-', '+', '.', ' '), '', $tel);
+
+        $preg = "/^0[23478][0-9]{8}$/";
+
+        return (preg_match($preg, $tel)) ? true : false;
+    }
+
+
+    /**
+     * Validate an Australian Company Number (ACN)
+     *
+     * @static
+     * @access  public
+     * @param   string  $acn    ACN to validate
+     * @return  bool
+     */
+    function acn( $acn )
+    {
+        
+        $digits = array();
+        
+        //Strip blanks
+        $acn = str_replace(array('(', ')', '-', '+', '.', ' '), '', $acn);
+        
+        //Check if contains only digits
+        if ( !is_numeric($acn) ) {
+            return false;
+        }
+        
+        //ABN has 9 digits in length
+        if ( strlen($acn) != 9 ) {
+            return false;
+        }
+        
+        //Put each digit to an array
+        for ( $i = 0; $i < strlen($acn); $i++ ) {
+            $digits[] = $acn[$i];
+        }
+        
+        $sum = 0;
+        //Apply weighting to digits 1 to 8
+        for ( $i = 0; $i < 8; $i++) {
+            $sum += $digits[$i]*(8-$i);
+        }
+        
+        //Divide by 10 to obtain remainder
+        $remainder = $sum%10;
+        
+
+        if ($remainder == 0) {
+            $complement = 0 - $remainder;
+        } else {
+            //Complement the remainder to 10
+            $complement = 10 - $remainder;
+        }
+        //$complement == last digit?
+        if ( $digits[8] == $complement ) {
+            return true;    
+        }
+        
+        return false;
+    }
+    
+    /**
+     * Social Security Number.
+     *
+     * Australia does not have a social security number system,
+     * the closest equivalent is a Tax File Number.
+     *
+     * @static
+     * @access  public
+     * @see     Validate_AU::tfn()
+     * @param   $input  Input to validate
+     * @return  bool
+     */
+    function ssn($input)
+    {
+        return Validate_AU::tfn($input);
+    }
+
+    /**
+     * Tax File Number (TFN)
+     *
+     * Australia does not have a social security number system,
+     * the closest equivalent is a Tax File Number.
+     *
+     * @static
+     * @access  public
+     * @param   $tfn    Tax File Number
+     * @return  bool
+     */
+    function tfn($tfn)
+    {   
+        $digits = array();
+        $weights = array(1, 4, 3, 7, 5, 8, 6, 9, 10);		
+
+        $tfn = str_replace(array('(', ')', '-', '+', '.', ' '), '', $tfn);
+
+        //Check if contains only digits
+        if ( !is_numeric($tfn) ) {
+            settype($tfn, "int");
+        }
+
+        //Check length
+        if ( strlen($tfn) < 8 || strlen($tfn) > 9 ) {
+            return false;
+        }
+
+        //Put each digit to an array
+        for ( $i = 0; $i < strlen($tfn); $i++ ) {
+            $digits[] = $tfn[$i];
+        }
+
+        $sum = 0;
+        //Apply weighting factor
+        //11 Digits
+        for ( $i = 0; $i < count($digits); $i++ ) {
+            $sum += $digits[$i]*$weights[$i];
+        }
+
+        $remainder = $sum%11;
+        
+        return !( $remainder > 0 );
+    }
+
+    /**
+     * Validate an Australian Business Number (ABN).
+     *
+     * You may optionally utilise an instance of Services_ABR to
+     * strictly validate input.
+     *
+     * @static
+     * @access  public
+     * @param   Services_ABR    $client An instance of a Services_ABR object with an api key set.
+     *
+     * @param   string          $abn    ABN to validate
+     * @param   bool            $strict Optionally do a strict check with a web service.
+     *                                  If you provide this param, you must provide the third param ($client).
+     * @return  bool|PEAR_Error
+     */
+    function abn($abn, $strict = false, $client = null)
+    {
+
+        $digits = array();
+        $weights = array(10, 1, 3, 5, 7, 9, 11, 13, 15, 17, 19);
+        
+        //Strip blanks
+        $abn = str_replace(array('(', ')', '-', '+', '.', ' '), '', $abn);
+        
+        //Check if contains only digits
+        if ( !is_numeric($abn) ) {
+            settype($abn, "int");
+            //return false;
+        }
+        
+        //ABN has 11 digits in length
+        if ( strlen($abn) != 11 ) {
+            return false;
+        }
+        
+        //Put each digit to an array
+        for ( $i = 0; $i < strlen($abn); $i++ ) {
+            $digits[] = $abn[$i];
+        }
+        
+        //Subtract 1 from the first left digit
+        $digits[0]--;
+        
+        $sum = 0;
+        //Apply weighting factor
+        //11 Digits
+        for ( $i = 0; $i < count($digits); $i++ ) {
+            $sum += $digits[$i]*$weights[$i];
+        }
+        
+        //Divide the total by 89 and get remainder
+        $remainder = $sum%89;
+        
+        if ( $remainder > 0 ) {
+            return false;
+        } else {
+
+            if ($strict) {
+                if (is_a($client,'Services_ABR')) {
+                    $result = $client->ABRSearchByABN($abn);
+
+                    if (PEAR::isError($result)) {
+                        return $result;
+                    } else {
+                        return true;
+                    }
+                } else {
+                    return PEAR::raiseError("Missing or invalid param provided");
+                }
+            }
+
+            return true;
+        }
+        
+    }
+}
+?>
---
+++ tests/validate_AU.phpt
@@ -0,0 +1,201 @@
+--TEST--
+validate_AU.phpt: Unit tests for 'Validate/AU.php'
+--FILE--
+<?php
+// 
+// Validate test script
+$noYes = array('NO', 'YES');
+require_once 'Validate/AU.php';
+
+echo "Test Validate_AU\n";
+echo "****************\n";
+
+$postalCodes = array( 5251, // OK
+                     5000, // OK
+                     4664, // OK
+                     2491, // OK
+                     1000, // NOK (OK if not strong)
+                     9999, // NOK (OK if not strong)
+                     'abc', // NOK
+                     'a7000' // NOK
+);
+    
+$abns = array( 
+'28 043 145 470',
+'65 497 794 289',
+'46 527 394 509',
+'99 070 045 359',
+'98 860 905 153',
+'53 106 288 699',
+'51 008 129 511',
+'43 500 713 236',
+'72 342 387 170',
+'21 188 299 895',
+'55 914 901 347',
+'92 638 328 368'
+ );//OK 
+
+
+$tfns = array( 
+'123 456 782'
+ );//OK 
+
+$acns = array(
+'000 000 019',
+'000 250 000',
+'000 500 005',
+'000 750 005',
+'001 000 004',
+'001 250 004',
+'001 500 009',
+'001 749 999',
+'001 999 999',
+'002 249 998',
+'002 499 998',
+'002 749 993',
+'002 999 993',
+'003 249 992',
+'003 499 992',
+'003 749 988',
+'003 999 988',
+'004 249 987',
+'004 499 987',
+'004 749 982',
+'004 999 982',
+'005 249 981',
+'005 499 981',
+'005 749 986',
+'005 999 977',
+'006 249 976',
+'006 499 976',
+'006 749 980',
+'006 999 980',
+'007 249 989',
+'007 499 989',
+'007 749 975',
+'007 999 975',
+'008 249 974',
+'008 499 974',
+'008 749 979',
+'008 999 979',
+'009 249 969',
+'009 499 969',
+'009 749 964',
+'009 999 964',
+'010 249 966',
+'010 499 966',
+'010 749 961'
+);//OK
+
+echo "\nTest postalCode without check against table\n";
+foreach ($postalCodes as $postalCode) {
+    echo "{$postalCode}: ".$noYes[Validate_AU::postalCode($postalCode)]."\n";
+}
+
+echo "\nTest postalCode with check against table (strong)\n";
+foreach ($postalCodes as $postalCode) {
+    echo "{$postalCode}: ".$noYes[Validate_AU::postalCode($postalCode, true)]."\n";
+}
+
+echo "\nTest abn\n";
+foreach ($abns as $abn) {
+    echo "{$abn}: ".$noYes[Validate_AU::abn($abn)]."\n";
+}
+
+echo "\nTest acn\n";
+foreach ($acns as $acn) {
+    echo "{$acn}: ".$noYes[Validate_AU::acn($acn)]."\n";
+}
+
+echo "\nTest tfn\n";
+foreach ($tfns as $tfn) {
+    echo "{$tfn}: ".$noYes[Validate_AU::tfn($tfn)]."\n";
+}
+
+?>
+--EXPECT--
+Test Validate_AU
+****************
+
+Test postalCode without check against table
+5251: YES
+5000: YES
+4664: YES
+2491: YES
+1000: YES
+9999: YES
+abc: NO
+a7000: NO
+
+Test postalCode with check against table (strong)
+5251: YES
+5000: YES
+4664: NO
+2491: NO
+1000: NO
+9999: NO
+abc: NO
+a7000: NO
+
+Test abn
+28 043 145 470: YES
+65 497 794 289: YES
+46 527 394 509: YES
+99 070 045 359: YES
+98 860 905 153: YES
+53 106 288 699: YES
+51 008 129 511: YES
+43 500 713 236: YES
+72 342 387 170: YES
+21 188 299 895: YES
+55 914 901 347: YES
+92 638 328 368: YES
+
+Test acn
+000 000 019: YES
+000 250 000: YES
+000 500 005: YES
+000 750 005: YES
+001 000 004: YES
+001 250 004: YES
+001 500 009: YES
+001 749 999: YES
+001 999 999: YES
+002 249 998: YES
+002 499 998: YES
+002 749 993: YES
+002 999 993: YES
+003 249 992: YES
+003 499 992: YES
+003 749 988: YES
+003 999 988: YES
+004 249 987: YES
+004 499 987: YES
+004 749 982: YES
+004 999 982: YES
+005 249 981: YES
+005 499 981: YES
+005 749 986: YES
+005 999 977: YES
+006 249 976: YES
+006 499 976: YES
+006 749 980: YES
+006 999 980: YES
+007 249 989: YES
+007 499 989: YES
+007 749 975: YES
+007 999 975: YES
+008 249 974: YES
+008 499 974: YES
+008 749 979: YES
+008 999 979: YES
+009 249 969: YES
+009 499 969: YES
+009 749 964: YES
+009 999 964: YES
+010 249 966: YES
+010 499 966: YES
+010 749 961: YES
+
+Test tfn
+123 456 782: YES
