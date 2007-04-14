1e8ed105290055b945feda788e5d78b006a17d2e
bertrand Gugger <toggg@php.net>
2007-04-14 16:23:33 +0000

    true is 8 bytes, better put some value in return
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@233765 c90b9560-bf6c-de11-be94-00142212c4b1

--- Validate/FR.php
+++ Validate/FR.php
@@ -257,25 +257,136 @@ class Validate_FR
     /**
      * Validates a French "departement"
      *
-     * @param string $region 2-digit department number
+     * @param string $region 2 (2A or 2B included) or 3 -digit department number
      * @return bool Whether the department is valid
      * @static
      */
     function region($region)
     {
+        static $return = array(
+            '01' => 'Ain',
+            '02' => 'Aisne',
+            '03' => 'Allier',
+            '04' => 'Alpes-de-Haute-Provence',
+            '05' => 'Hautes-Alpes',
+            '06' => 'Alpes-Maritimes',
+            '07' => 'Ardèche',
+            '08' => 'Ardennes',
+            '09' => 'Ariège',
+            '10' => 'Aube',
+            '11' => 'Aude',
+            '12' => 'Aveyron',
+            '13' => 'Bouches-du-Rhône',
+            '14' => 'Calvados',
+            '15' => 'Cantal',
+            '16' => 'Charente',
+            '17' => 'Charente-Maritime',
+            '18' => 'Cher',
+            '19' => 'Corrèze',
+            '20' => 'Corse',
+            '21' => 'Côte-d\'Or',
+            '22' => 'Côtes d\'Armor',
+            '23' => 'Creuse',
+            '24' => 'Dordogne',
+            '25' => 'Doubs',
+            '26' => 'Drôme',
+            '27' => 'Eure',
+            '28' => 'Eure-et-Loir',
+            '29' => 'Finistère',
+            '2A' => 'Corse-du-Sud',
+            '2B' => 'Haute-Corse',
+            '30' => 'Gard',
+            '31' => 'Haute-Garonne',
+            '32' => 'Gers',
+            '33' => 'Gironde',
+            '34' => 'Hérault',
+            '35' => 'Ille-et-Vilaine',
+            '36' => 'Indre',
+            '37' => 'Indre-et-Loire',
+            '38' => 'Isère',
+            '39' => 'Jura',
+            '40' => 'Landes',
+            '41' => 'Loir-et-Cher',
+            '42' => 'Loire',
+            '43' => 'Haute-Loire',
+            '44' => 'Loire-Atlantique',
+            '45' => 'Loiret',
+            '46' => 'Lot',
+            '47' => 'Lot-et-Garonne',
+            '48' => 'Lozère',
+            '49' => 'Maine-et-Loire',
+            '50' => 'Manche',
+            '51' => 'Marne',
+            '52' => 'Haute-Marne',
+            '53' => 'Mayenne',
+            '54' => 'Meurthe-et-Moselle',
+            '55' => 'Meuse',
+            '56' => 'Morbihan',
+            '57' => 'Moselle',
+            '58' => 'Nièvre',
+            '59' => 'Nord',
+            '60' => 'Oise',
+            '61' => 'Orne',
+            '62' => 'Pas-de-Calais',
+            '63' => 'Puy-de-Dôme',
+            '64' => 'Pyrénées-Atlantiques',
+            '65' => 'Hautes-Pyrénées',
+            '66' => 'Pyrénées-Orientales',
+            '67' => 'Bas-Rhin',
+            '68' => 'Haut-Rhin',
+            '69' => 'Rhône',
+            '70' => 'Haute-Saône',
+            '71' => 'Saône-et-Loire',
+            '72' => 'Sarthe',
+            '73' => 'Savoie',
+            '74' => 'Haute-Savoie',
+            '75' => 'Paris',
+            '76' => 'Seine-Maritime',
+            '77' => 'Seine-et-Marne',
+            '78' => 'Yvelines',
+            '79' => 'Deux-Sèvres',
+            '80' => 'Somme',
+            '81' => 'Tarn',
+            '82' => 'Tarn-et-Garonne',
+            '83' => 'Var',
+            '84' => 'Vaucluse',
+            '85' => 'Vendée',
+            '86' => 'Vienne',
+            '87' => 'Haute-Vienne',
+            '88' => 'Vosges',
+            '89' => 'Yonne',
+            '90' => 'Territoire de Belfort',
+            '91' => 'Essonne',
+            '92' => 'Hauts-de-Seine',
+            '93' => 'Seine-St-Denis',
+            '94' => 'Val-de-Marne',
+            '95' => 'Val-D\'Oise',
+            '971' => 'Guadeloupe',
+            '972' => 'Martinique',
+            '973' => 'Guyane',
+            '974' => 'Réunion',
+            '975' => 'Saint-Pierre-et-Miquelon',
+            '976' => 'Mayotte',
+            '984' => 'Terres Australes et Antarctiques',
+            '985' => 'Mayotte', // deprecated
+            '986' => 'Wallis-et-Futuna',
+            '987' => 'Polynésie française',
+            '988' => 'Nouvelle-Calédonie'
+        );
+    
         if (is_numeric($region)
             && floor($region) == ceil($region)) {
             switch (strlen($region)) {
                 case 2:
                     if ($region >= 1 && $region <= 95
                         && $region != 20) {
-                        return true;
+                        return $return[$region] || true;
                     }
                     break;
                 case 3:
                     /* DOM/TOM et collectivitees OM  */
-                    if (($region >= 971 && $region <= 975) || ($region >= 984 && $region <= 988)) {
-                        return true;
+                    if (($region >= 971 && $region <= 976) || ($region >= 984 && $region <= 988)) {
+                        return $return[$region] || true;
                     }
                     break;
             }
@@ -283,7 +394,7 @@ class Validate_FR
         switch (strtoupper($region)) {
             case '2A':
             case '2B':
-                return true;
+                return $return[$region] || true;
         }
         return false;
     }
