1bc0922f9b82cb6e052578e5f4aa912d1b37e11d
Ken Guest <kguest@php.net>
2007-08-12 23:48:22 +0000

    fix for Bug #11825  phpcs/Coding Standards issues highlighted in DK.php
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@241151 c90b9560-bf6c-de11-be94-00142212c4b1

--- Validate/DK.php
+++ Validate/DK.php
@@ -1,30 +1,26 @@
 <?php
 
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2005 Jesper Veggerby Hansen                       |
-// +----------------------------------------------------------------------+
-// | This source file is subject to the New BSD license, That is bundled  |
-// | with this package in the file LICENSE, and is available through      |
-// | the world-wide-web at                                                |
-// | http://www.opensource.org/licenses/bsd-license.php                   |
-// | If you did not receive a copy of the new BSDlicense and are unable   |
-// | to obtain it through the world-wide-web, please send a note to       |
-// | pajoye@php.net so we can mail you a copy immediately.                |
-// +----------------------------------------------------------------------+
-// | Author:  Jesper Veggerby <pear.nosey@veggerby.dk>                    |
-// +----------------------------------------------------------------------+
-//
 /**
  * Specific validation methods for data used in DK
  *
- * @category   Validate
- * @package    Validate_DK
- * @author     Jesper Veggerby <pear.nosey@veggerby.dk>
- * @copyright  Copyright (C) 2003, 2004, 2005 Jesper Veggerby Hansen
- * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
- * @version    CVS: $Id$
- * @link       http://pear.php.net/package/Validate_DK
+ * PHP Versions 4 and 5
+ *
+ * This source file is subject to the New BSD license, That is bundled
+ * with this package in the file LICENSE, and is available through
+ * the world-wide-web at
+ * http://www.opensource.org/licenses/bsd-license.php
+ * If you did not receive a copy of the new BSDlicense and are unable
+ * to obtain it through the world-wide-web, please send a note to
+ * pajoye@php.net so we can mail you a copy immediately.
+ *
+ * @category  Validate
+ * @package   Validate_DK
+ * @author    Jesper Veggerby <pear.nosey@veggerby.dk>
+ * @copyright 2003-2005 Jesper Veggerby Hansen
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   CVS: $Id$
+ * @link      http://pear.php.net/package/Validate_DK
  */
 
 /**
@@ -37,30 +33,32 @@
  * - Danish telephone number
  * - Car registration number
  *
- * @category   Validate
- * @package    Validate_DK
- * @author     Jesper Veggerby <pear.nosey@veggerby.dk>
- * @copyright  Copyright (C) 2003, 2004, 2005 Jesper Veggerby Hansen
- * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
- * @version    Release: @package_version@
- * @link       http://pear.php. net/package/Validate_DK
+ * @category  Validate
+ * @package   Validate_DK
+ * @author    Jesper Veggerby <pear.nosey@veggerby.dk>
+ * @copyright 2003-2005 Jesper Veggerby Hansen
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   Release: @package_version@
+ * @link      http://pear.php. net/package/Validate_DK
  */
 class Validate_DK
 {
 
     /**
      * validates a postcode
-	 *
+     *
      * Four digit postal code, maybe with a leading 'DK-'
      *
+     * @param string $postcode the postcode to be validated
+     * @param bool   $strong   optional; strong checks (e.g. against a list 
+     *                         of postcodes)
+     *
      * @access    public
-     * @param     string  the postcode to be validated
-     * @param     bool    optional; strong checks (e.g. against a list of postcodes)
      * @return    bool
      */
     function postalCode($postcode, $strong=false)
     {
-        $preg = "/^(DK-)?[0-9]{4}$/";
+        $preg  = "/^(DK-)?[0-9]{4}$/";
         $match = preg_match($preg, $postcode)? true : false;
         return $match;
     }
@@ -77,19 +75,22 @@ class Validate_DK
      * The last digit of the control number (y) is also dependend on gender, if
      * y is odd it's a male cpr number and if even a female.
      *
+     * @param string $cpr    CPR number
+     * @param string $gender The gender to validate for 'M' for male, 'F'
+     *                       for female, false or omitted to not perform 
+     *                       the check.
+     *
      * @access    public
-     * @param     string $cpr CPR number
-     * @param     string $gender The gender to validate for 'M' for male, 'F'
-     * for female, false or omitted to not perform the check
      * @return    bool
      */
-    function ssn($cpr, $gender = false){
+    function ssn($cpr, $gender = false)
+    {
         static $control = array(4, 3, 2, 7, 6, 5, 4, 3, 2, 1);
 
         // remove spaces and uppercase it
         $preg = "/^[0-9]{6}\-?[0-9]{4}$/";
         if (preg_match($preg, $cpr)) {
-            $cpr = str_replace('-', '', $cpr);
+            $cpr           = str_replace('-', '', $cpr);
             $controlCipher = 0;
             for ($i = 0; $i < count($control); $i++) {
                 $controlCipher += $control[$i] * substr($cpr, $i, 1);
@@ -117,16 +118,17 @@ class Validate_DK
      *
      * Simple check: 8 digits when removing (, ), -, +, ., ' '
      *
+     * @param string $tel the tel number
+     *
      * @access    public
-     * @param     string $tel the tel number
      * @return    bool
-     * @see
      */
-    function phoneNumber($tel){
+    function phoneNumber($tel)
+    {
         // just checks to see if it is numeric and starts with a 0
         // remove any wierd characters like (,),-,. etc
-        $tel = str_replace(Array('(', ')', '-', '+', '.', ' '), '', $tel);
-        $preg = "/^[0-9]{8}$/";
+        $tel   = str_replace(Array('(', ')', '-', '+', '.', ' '), '', $tel);
+        $preg  = "/^[0-9]{8}$/";
         $match = (preg_match($preg, $tel)) ? true : false;
         return $match;
     }
@@ -138,12 +140,13 @@ class Validate_DK
      *
      * Where AA are 2 letter UPPERCASE A-Z
      *
+     * @param string $reg the registration number
+     *
      * @access    public
-     * @author    Michael Dransfield <mikeNO@SPAMblueroot.net>
-     * @param     string $reg the registration number
      * @return    bool
      */
-    function carReg($reg){
+    function carReg($reg)
+    {
         $prepreg = "/^[A-Z]{2} [0-9]{2} [0-9]{3}$/";
         if (preg_match($prepreg, $reg)) {
             return true;
