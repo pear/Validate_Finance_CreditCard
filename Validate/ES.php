24538ab93fc22e639655b9a3aa025fc509bcfc08
Daniel O'Connor <clockwerx@php.net>
2010-06-02 23:35:37 +0000

    Request #17454 	Add check for CIF
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@300116 c90b9560-bf6c-de11-be94-00142212c4b1

--- Validate/ES.php
+++ Validate/ES.php
@@ -41,15 +41,15 @@ require_once 'Validate.php';
  * This class provides methods to validate:
  *  - Spanish DNI number ("El documento de la identificación nacional")
  *
- * @category   Validate
- * @package    Validate_ES
- * @author     Tomas V.V.Cox <cox@idecnet.com>
- * @author     Byron Adams <byron.adams54@gmail.com>
- * @copyright  1997-2005 Pierre-Alain Joye, Tomas V.V.Cox
- * @copyright  (c) 2006 Byron Adams
- * @license    http://www.opensource.org/licenses/bsd-license.php  New BSD License
- * @version    Release: @package_version@
- * @link       http://pear.php.net/package/Validate_ES
+ * @category  Validate
+ * @package   Validate_ES
+ * @author    Tomas V.V.Cox <cox@idecnet.com>
+ * @author    Byron Adams <byron.adams54@gmail.com>
+ * @copyright 1997-2005 Pierre-Alain Joye, Tomas V.V.Cox
+ * @copyright (c) 2006 Byron Adams
+ * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
+ * @version   Release: @package_version@
+ * @link      http://pear.php.net/package/Validate_ES
  */
 class Validate_ES
 {
@@ -59,8 +59,8 @@ class Validate_ES
     * In Spain, all Spanish citizens are issued with a DNI 
     * the numbers are used as identification for almost all purposes.
     * 
-    * @param     string    $dni El Documento Nacional de Indentidad a chequear
-    * @return    bool      returns true on success false otherwise
+    * @param string $dni El Documento Nacional de Indentidad a chequear
+    * @return bool returns true on success false otherwise
     * @author    Tomas V.V.Cox <cox@idecnet.com>
     * @author    Byron Adams <byron.adams54@gmail.com>
     * @link      http://es.wikipedia.org/wiki/Algoritmo_para_obtener_la_letra_del_NIF
@@ -68,20 +68,81 @@ class Validate_ES
     */
     function dni($dni)
     {
-        $dni = str_replace("-","",trim($dni));
+        $dni = str_replace("-", "", trim($dni));
         $letters = 'TRWAGMYFPDXBNJZSQVHLCKET';   
        
         $start = (int)(strtoupper($dni{0}) == "X");
         
-        $number = substr($dni,$start,-1);
+        $number = substr($dni, $start, -1);
         $letter = strtoupper(substr($dni, -1));
         
-        if(!ctype_digit($number) ||
-           !ctype_alpha($letter)) {
+        if (!ctype_digit($number) || !ctype_alpha($letter)) {
            return false;
         }
         
         return ($letter == $letters{$number % 23});
     }
+
+    /**
+     * Validate Spanish CIF number
+     *
+     * In Spain, all Spanish companies are issued with a CIF code.
+     * 
+     * @param string $cif CIF to check
+     *
+     * @return bool returns true on success false otherwise
+     */
+    function cif($cif)
+    {
+        $cif = strtoupper(str_replace("-", "", trim($cif)));
+
+        $letters = 'ABCDEFGHJKLMNPRQSUVW';   
+
+        if (preg_match("/^[$letters]\d{7}[\d[ABCDEFGHIJ]$/", $cif) == 0) {
+            return false;
+        }
+
+        $letter = substr($cif, 0, 1);
+
+        $provinceCode = substr($cif, 1, 2);
+
+        $number = substr($cif, 3, 5);
+        $controlCode = substr($cif, 8, 1);
+
+        if (strpos($letter, "KPQS")!=0 && ctype_digit($controlCode)) {
+            return false;
+        }
+
+        if (strpos($letter, "ABEH")!=0 && ctype_alpha($controlCode)) {
+            return false;
+        }
+
+        $a = $provinceCode[1]+$number[1]+$number[3];
+
+        $b = 0;
+        $oddNumbers = array($provinceCode[0], $number[0], $number[2], $number[4]);
+        foreach ($oddNumbers as $number) {
+            $x = $number*2;
+            if ($x >= 10) {
+                $x = ($x % 10)+1;
+            }
+            $b += $x;
+        }
+
+        $c = $a + $b;
+
+        $e = $c % 10;
+
+        if ($e != 0) {
+            $d = 10-$e;
+        } else {
+            $d = 0;
+        }
+
+        if ((int)$controlCode != $d && $letters2[$controlCode] != $d) {
+            return false;
+        }
+
+        return true;
+    }
 }
-?>
