8968f0f94b2a0fb9b63d069e01a0defbd63a95f1
Daniel O'Connor <clockwerx@php.net>
2010-08-23 10:54:27 +0000

    Avoid split()
    git-svn-id: http://svn.php.net/repository/pear/packages/Validate/trunk@302685 c90b9560-bf6c-de11-be94-00142212c4b1

--- Validate/esMX.php
+++ Validate/esMX.php
@@ -148,7 +148,7 @@ class Validate_EsMX
                 $digitModule        = '';
                 $digitVerifier      = '';
                 $combinations       = '0123456789ABCDEFGHIJKLMN-OPQRSTUVWXYZ*';
-                $combinationsValues = split(',', '00,01,02,03,04,05,06,07,08,09'.
+                $combinationsValues = explode(',', '00,01,02,03,04,05,06,07,08,09'.
                                             ',10,11,12,13,14,15,16,17,18,19,20,'.
                                             '21,22,23,24,25,26,27,28,29,30,31,'.
                                             '32,33,34,35,36,37');
